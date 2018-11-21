aa0880e18aabd91322a1ca558b75f5dedaeedb39
matijagrdjan <matija.grdjan@gmail.com>
2018-11-21 14:05:39 +0100

    Initial Project Add

---
+++ src/test/java/hr/grdjan/matija/sfgpetclinc/SfgPetClincApplicationTests.java
@@ -0,0 +1,16 @@
+package hr.grdjan.matija.sfgpetclinc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfgPetClincApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
