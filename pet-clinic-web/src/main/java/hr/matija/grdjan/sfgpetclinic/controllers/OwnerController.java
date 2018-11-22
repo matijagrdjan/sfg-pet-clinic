e9e3e3427afcb81c8679eff4ae38c180be8d4043
matijagrdjan <matija.grdjan@gmail.com>
2018-11-22 13:38:45 +0100

    Owner controller and index page

---
+++ pet-clinic-web/src/main/java/hr/matija/grdjan/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,12 @@
+package hr.matija.grdjan.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RequestMapping({"/owner"})
+@Controller
+public class OwnerController {
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listVets(){
+        return "owner/index";
+    }
+}
