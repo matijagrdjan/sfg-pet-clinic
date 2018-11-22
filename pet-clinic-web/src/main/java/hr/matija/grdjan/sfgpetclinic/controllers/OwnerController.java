d9c923c5d509b480b8a556c46eca65143aed8ca6
matijagrdjan <matija.grdjan@gmail.com>
2018-11-22 15:29:53 +0100

    Add owner list to xml and update controller

--- pet-clinic-web/src/main/java/hr/matija/grdjan/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/hr/matija/grdjan/sfgpetclinic/controllers/OwnerController.java
@@ -1,12 +1,22 @@
 package hr.matija.grdjan.sfgpetclinic.controllers;
 
+import hr.matija.grdjan.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping({"/owner"})
 @Controller
 public class OwnerController {
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    private final OwnerService ownerService;
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("owners",ownerService.findAll());
         return "owner/index";
     }
 }
