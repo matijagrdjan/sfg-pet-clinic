25a16dfd3d1993a2805516260dab2c1de7591062
matijagrdjan <matija.grdjan@gmail.com>
2018-11-26 11:42:57 +0100

    Added Specialties Map Impl

---
+++ pet-clinic-data/src/main/java/hr/matija/grdjan/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package hr.matija.grdjan.sfgpetclinic.services;
+
+import hr.matija.grdjan.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudServices<Speciality,Long> {
+}
