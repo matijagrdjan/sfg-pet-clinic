c524a3ac5d4fc864c18dcfb607c6ceac1f4c72c3
matijagrdjan <matija.grdjan@gmail.com>
2018-11-28 15:20:35 +0100

    lombok model

--- pet-clinic-data/src/main/java/hr/matija/grdjan/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/hr/matija/grdjan/sfgpetclinic/model/PetType.java
@@ -1,21 +1,33 @@
 package hr.matija.grdjan.sfgpetclinic.model;
 
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
+
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
     @Column(name = "name")
     private String name;
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public String toString() {
+        return name;
     }
 }
